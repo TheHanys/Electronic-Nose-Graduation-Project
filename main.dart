5577b6735860b123a1aadf36ea6d933b58d020b2
Omar EL-badry <66132636+omarbaadry@users.noreply.github.com>
2022-03-05 02:21:26 +0200

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,31 @@
+import 'package:enos_app/home/wrapper.dart';
+import 'package:enos_app/models/user.dart';
+import 'package:enos_app/screens/home_page.dart';
+import 'package:enos_app/services/auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        StreamProvider<Myuser?>.value(
+          value: AuthService().user,
+          catchError: (_, __) {},
+          initialData: null,
+        ),
+      ],
+      child: MaterialApp(debugShowCheckedModeBanner: false, home: Wrapper()),
+    );
+  }
+}
